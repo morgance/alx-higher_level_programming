074fb2da8ef322af8b65d0f2e688d232993073db
morgance <mogeokumu@gmail.com>
2023-06-08 06:40:50 -0700

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     import hidden_4
 
     for name in dir(hidden_4):
-        if name[0] != ' ' and name[1] != ' ':
+        if name[0] != '_' and name[1] != '_':
             print(name)
