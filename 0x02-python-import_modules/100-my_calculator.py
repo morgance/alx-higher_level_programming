7a15a42ec5e140d8b9a5c69bc8d3c8ec304ef29a
morgance <mogeokumu@gmail.com>
2023-06-08 13:46:55 -0700

    debugging

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -24,4 +24,4 @@ if __name__ == "__main__":
     elif sums == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
-        print("{} / {} = {}".format(a, b, div(a, b )))
+        print("{} / {} = {}".format(a, b, div(a, b)))
