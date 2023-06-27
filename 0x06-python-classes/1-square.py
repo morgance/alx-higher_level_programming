c74616c9786cce5ae0a420b801516ed95696a243
morgance <mogeokumu@gmail.com>
2023-06-27 07:11:09 -0700

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
  """Class Square defines size"""
 
-
-
- class Square:
+class Square:
     """Class Square defines a square size"""
     def __init__(self, size):
         """Initialize method stores the size"""
