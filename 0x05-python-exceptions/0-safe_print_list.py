45ca75932abf61572713a0a29bd70f71a6223449
morgance <mogeokumu@gmail.com>
2023-06-27 01:53:38 -0700

    Corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             num += 1
-        except:
+        except IndexError:
             break
-    
     print("")
     return (num)
