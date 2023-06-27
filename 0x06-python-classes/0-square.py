a75a214586fd3c262cc99466eba82f80e4073d15
morgance <mogeokumu@gmail.com>
2023-06-27 06:30:43 -0700

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Empty class that defines a square"""
-
+""" Empty class that defines a 
+square"""
 class square:
-    """Empty class that defines a square"""
+    """Empty class that defines a 
+    square"""
     pass
