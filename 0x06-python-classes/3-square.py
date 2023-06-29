a5414ff433d428321a1b4b811c6b96e30a09ef42
morgance <mogeokumu@gmail.com>
2023-06-29 01:00:50 -0700

    debugging

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """class with size and method of ares"""
+
+
 class Square:
 
-
     """class that defines square by size"""
+
     def __init__(self, size=0):
         """ Method to contruct the square object"""
         if not isinstance(size, int):
