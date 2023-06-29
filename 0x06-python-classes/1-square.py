05fc244445b1630f1fa594a92cd694d63536206d
morgance <mogeokumu@gmail.com>
2023-06-29 00:56:00 -0700

    corrections

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Class Square defines size"""
 
+
 class Square:
 
-
     """Class Square defines a square size"""
+
     def __init__(self, size):
         """Constructor of the size"""
         self.__size = size
