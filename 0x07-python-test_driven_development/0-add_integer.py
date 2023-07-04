c0b656d7f18036b9469c050dbecc69154d5a16eb
morgance <mogeokumu@gmail.com>
2023-07-04 15:17:22 -0700

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Accepts two values and casts them into integers
+Returns the sum of those integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Returns an integer sum
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return (int(a) + int(b))
