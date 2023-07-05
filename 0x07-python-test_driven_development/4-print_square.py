90c147085f8a257a487d2236f311c60077c2535a
morgance <mogeokumu@gmail.com>
2023-07-05 03:02:04 -0700

    debigging

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
 """
-Prints a square with character "#"
+
+function that prints a square with the character #
+
 """
 
 
 def print_square(size):
+    """ Function that prints a square with the character #
+
+    Args:
+        size: size of the square printed
+
+    Returns:
+        No return
+
+    Raises:
+        TypeError: If size is not an integer number
+
+
     """
-    Returns a square with # character
-    """
-    if not isinstance(size, (int, float)):
+
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size == 0:
-        print("", end="")
-
-    else:
-        print("\n".join(["#" * size for rows in range(size)]))
+    for i in range(size):
+        print("#" * size)
