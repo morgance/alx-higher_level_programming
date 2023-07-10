40e620c737d8a7994a6085940af2620ae13170b3
morgance <mogeokumu@gmail.com>
2023-07-10 14:45:26 -0700

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""unction that returns True if the object is an instance
+of a class that inherited (directly or indirectly) from
+the specified class ; otherwise False."""
+
+
+def inherits_from(obj, a_class):
+    """ Function that returns True/False if object
+
+    Args:
+        obj: object
+        a_class: class type
+
+    Returns:
+        True if obj is an instance of a_class
+        False, otherwise
+    """
+    if type(obj) is a_class:
+        return False
+    return isinstance(obj, a_class)
