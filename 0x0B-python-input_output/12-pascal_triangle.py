6310f28d41768b76e530a9b533cd5e30b1672bf7
morgance <mogeokumu@gmail.com>
2023-07-11 15:00:13 -0700

    Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,23 +3,16 @@
 
 
 def pascal_triangle(n):
-    """ Function that returns the pascal triangle"""
+    """Returns pascal's trangle """
+    if n <= 0:
+        return []
 
-    pro = []
-    pyst = []
-
-    for i in range(n):
-        list_ls = []
-        p1 = -1
-        p2 = 0
-        for j in range(len(pyst) + 1):
-            if p1 == -1 or p2 == len(prev):
-                list_ls += [1]
-            else:
-                list_ls += [pyst[p1] + pyst[p2]]
-            p1 += 1
-            p2 += 1
-        pro.append(list_ls)
-        pyst = list_ls[:]
-
-    return pro
+    triangles = [[1]]
+    while len(triangles) != n:
+        pro = triangles[-1]
+        tsr = [1]
+        for i in range(len(pro) - 1):
+            tsr.append(pro[i] + pro[i + 1])
+        tsr.append(1)
+        triangles.append(tsr)
+    return triangles
