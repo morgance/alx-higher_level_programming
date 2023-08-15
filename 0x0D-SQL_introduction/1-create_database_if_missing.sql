25a24c470c11c99448effae1f959651e33cdc709
morgance <mogeokumu@gmail.com>
2023-08-15 14:04:04 -0700

    Create a database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 --creates the database hbtn_0c_0
---checks if Database already exists
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+--checks if Database already exists in MSQL
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
