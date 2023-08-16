bf23f71918aa10ae2644785ea559db138aa87379
morgance <mogeokumu@gmail.com>
2023-08-16 15:02:48 -0700

    Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Script that lists all the cities of California registered in the database
+SELECT id,name
+FROM cities
+WHERE state_id = ( -- Query to get the id of California
+      SELECT id
+      FROM states
+      WHERE name = "California");
