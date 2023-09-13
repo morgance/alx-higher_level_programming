131ad65b5961cc644cd9662094aece12bd5a1ef5
morgance <mogeokumu@gmail.com>
2023-09-13 07:12:46 -0700

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-const newDict = {};
-
-Object.keys(dict).map(function (key, index) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [];
+const newDic = {};
+for (const key in dict) {
+  if (newDic[dict[key]] === undefined) {
+    newDic[dict[key]] = [];
   }
-  newDict[dict[key]].push(key);
-});
+  newDic[dict[key]].push(key);
+}
 
-console.log(newDict);
+console.log(newDic);
