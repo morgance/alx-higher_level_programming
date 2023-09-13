91d8a984850d65c3cb92268cb55ba4e17b5596d3
morgance <mogeokumu@gmail.com>
2023-09-13 06:50:33 -0700

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
-!/usr/bin/node
-const list = require('./100-data.js').list;
-const newList = list.map(functio (num, index) {
-  return num * index;
-});
+#!/usr/bin/node
+
+const { list } = require('./100-data.js');
+
+const newList = list.map((value, index) => value * index);
 
 console.log(list);
 console.log(newList);
