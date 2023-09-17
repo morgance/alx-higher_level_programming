4db3a1b71532ae3b85c5e0c3ff260e697aaf510d
morgance <mogeokumu@gmail.com>
2023-09-17 12:21:39 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/node
 """
-Lists all states with a name starting with N
+states with a name starting with N
 """
 from sys import argv
-import MyQLdb
+import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(
