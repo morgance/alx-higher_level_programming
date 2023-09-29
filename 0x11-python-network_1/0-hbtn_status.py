85e74e894e4ff46388a23395f1010f179ade1757
morgance <mogeokumu@gmail.com>
2023-09-29 07:10:24 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" script that fetches https://intranet.hbtn.io/status """
+"""script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
